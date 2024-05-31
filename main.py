46ee6778265ba50e9eac16053d72fbfdbab38743
Vishesh Paliwal <vishesh.23bcs10013@ms.sst.scaler.com>
2024-05-31 18:49:17 +0530

    creates result file as well

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-"""
-Main script to scrape the comments of any Youtube video.
-
-Example:
-    $ python main.py YOUTUBE_VIDEO_URL
-"""
-
 import csv
 import io
-from selenium import webdriver
-from selenium.common import exceptions
 import sys
 import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.common import exceptions
+import chromedriver_autoinstaller
 
 def scrape(url):
     """
@@ -24,37 +20,34 @@ def scrape(url):
         When certain elements to look for cannot be found
     """
 
-    # Note: Download and replace argument with path to the driver executable.
-    # Simply download the executable and move it into the webdrivers folder.
-    driver = webdriver.Chrome('./webdrivers/chromedriver')
+    # Automatically download and install the correct version of chromedriver
+    chromedriver_autoinstaller.install()
 
-    # Navigates to the URL, maximizes the current window, and
-    # then suspends execution for (at least) 5 seconds (this
-    # gives time for the page to load).
+    # Initialize the Chrome driver
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(options=options)
+
+    # Navigates to the URL and suspends execution for (at least) 5 seconds (this gives time for the page to load).
     driver.get(url)
-    driver.maximize_window()
     time.sleep(5)
 
     try:
-        # Extract the elements storing the video title and
-        # comment section.
-        title = driver.find_element_by_xpath('//*[@id="container"]/h1/yt-formatted-string').text
-        comment_section = driver.find_element_by_xpath('//*[@id="comments"]')
+        # Extract the elements storing the video title and comment section.
+        title = driver.find_element(By.XPATH, '//*[@id="container"]/h1/yt-formatted-string').text
+        comment_section = driver.find_element(By.XPATH, '//*[@id="comments"]')
     except exceptions.NoSuchElementException:
-        # Note: Youtube may have changed their HTML layouts for
-        # videos, so raise an error for sanity sake in case the
-        # elements provided cannot be found anymore.
-        error = "Error: Double check selector OR "
-        error += "element may not yet be on the screen at the time of the find operation"
+        # Note: Youtube may have changed their HTML layouts for videos, so raise an error for sanity sake in case the elements provided cannot be found anymore.
+        error = "Error: Double check selector OR element may not yet be on the screen at the time of the find operation"
         print(error)
+        driver.close()
+        return
 
-    # Scroll into view the comment section, then allow some time
-    # for everything to be loaded as necessary.
+    # Scroll into view the comment section, then allow some time for everything to be loaded as necessary.
     driver.execute_script("arguments[0].scrollIntoView();", comment_section)
     time.sleep(7)
 
-    # Scroll all the way down to the bottom in order to get all the
-    # elements loaded (since Youtube dynamically loads them).
+    # Scroll all the way down to the bottom in order to get all the elements loaded (since Youtube dynamically loads them).
     last_height = driver.execute_script("return document.documentElement.scrollHeight")
 
     while True:
@@ -75,17 +68,18 @@ def scrape(url):
 
     try:
         # Extract the elements storing the usernames and comments.
-        username_elems = driver.find_elements_by_xpath('//*[@id="author-text"]')
-        comment_elems = driver.find_elements_by_xpath('//*[@id="content-text"]')
+        username_elems = driver.find_elements(By.XPATH, '//*[@id="author-text"]')
+        comment_elems = driver.find_elements(By.XPATH, '//*[@id="content-text"]')
     except exceptions.NoSuchElementException:
-        error = "Error: Double check selector OR "
-        error += "element may not yet be on the screen at the time of the find operation"
+        error = "Error: Double check selector OR element may not yet be on the screen at the time of the find operation"
         print(error)
+        driver.close()
+        return
 
     print("> VIDEO TITLE: " + title + "\n")
 
     with io.open('results.csv', 'w', newline='', encoding="utf-16") as file:
-        writer = csv.writer(file, delimiter =",", quoting=csv.QUOTE_ALL)
+        writer = csv.writer(file, delimiter=",", quoting=csv.QUOTE_ALL)
         writer.writerow(["Username", "Comment"])
         for username, comment in zip(username_elems, comment_elems):
             writer.writerow([username.text, comment.text])
@@ -93,4 +87,8 @@ def scrape(url):
     driver.close()
 
 if __name__ == "__main__":
-    scrape(sys.argv[1])
+    if len(sys.argv) < 2:
+        print("Error: Please provide a YouTube video URL as an argument.")
+        print("Usage: python main.py YOUTUBE_VIDEO_URL")
+    else:
+        scrape(sys.argv[1])
